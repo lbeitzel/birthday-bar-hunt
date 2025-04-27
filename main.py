6d65562ffa94bf7fb8b049c1847ccdf1f8609d9c
Lucas <lucasbeitzel@gmail.com>
2025-04-26 22:52:10 -0500

    flattened proj structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from . import db
+from app import db
 
 
 main = Blueprint('main', __name__)
