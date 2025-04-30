06f786589bd047383e110ef836a93ffe96ca2476
Lucas <lucasbeitzel@gmail.com>
2025-04-30 09:00:35 -0500

    added question options

--- project/main.py
+++ project/main.py
@@ -1,19 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
-from .models import Question
+from .models import Question, Option
 
 
 main = Blueprint('main', __name__)
-
-OPTIONS = {
-    "green": "Green",
-    "purple": "Purple", 
-    "red": "Red",
-    "yellow": "Yellow",
-    "blue": "Blue"
-}
-
 
 @main.route('/')
 def index():
@@ -28,14 +19,17 @@ def profile():
 @main.route("/q/<number>")
 @login_required
 def question(number=1):
-    # first_q = Question(hint1="What is the Lucas's favorite color?", hint2="Another name for emerald, jade, seafoam?", hint3="What color is grass?", answer="green")
-    # db.session.add(first_q)
+    # ### method for creating a new DB via model definition
+    # options = Option(option1="Green", option2="Purple", option3="Red", option4="Yellow", option5="Blue")
+    # db.session.add(options)
     # db.session.commit()
 
     question = Question.query.filter_by(id=number).first()
+    options = Option.query.filter_by(id=current_user.id).first()
+    question_options = [options.option1, options.option2, options.option3, options.option4, options.option5]
 
     if number == str(current_user.questionNum):
-        return render_template("question.html", number=current_user.questionNum, hintNum=current_user.hintNum, hint1=question.hint1, hint2=question.hint2, hint3=question.hint3, answer=question.answer, options=OPTIONS)
+        return render_template("question.html", number=current_user.questionNum, hintNum=current_user.hintNum, hint1=question.hint1, hint2=question.hint2, hint3=question.hint3, answer=question.answer, options=question_options)
     else:
         return "Error wrong question page"
 
@@ -44,18 +38,9 @@ def question(number=1):
 @login_required
 def result():
     selected = request.form.get('option')
-
     question = Question.query.filter_by(id=current_user.questionNum).first()
-    
     correctAns = False
-
-    # Check if an option was selected
-    if not selected or selected not in OPTIONS:
-        return redirect(url_for('main.question', number=current_user.questionNum))
     
-    # Get the display name of the option
-    option_name = OPTIONS.get(selected)
-
     print(f"Correct answer: {question.answer}")
     print(f"Chosen answer: {selected}")
 
@@ -65,4 +50,4 @@ def result():
         correctAns = True
         # current_user.questionNum += 1
     
-    return render_template('result.html', option=selected, option_name=option_name, q=current_user.questionNum, correctAns=correctAns)
+    return render_template('result.html', choice=selected, q=current_user.questionNum, correctAns=correctAns)
--- project/models.py
+++ project/models.py
@@ -16,3 +16,12 @@ class Question(UserMixin, db.Model):
     hint2 = db.Column(db.String(200))
     hint3 = db.Column(db.String(200))
     answer = db.Column(db.String(100))
+
+
+class Option(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    option1 = db.Column(db.String(100))
+    option2 = db.Column(db.String(100))
+    option3 = db.Column(db.String(100))
+    option4 = db.Column(db.String(100))
+    option5 = db.Column(db.String(100))
